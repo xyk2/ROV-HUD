71f9f4f952580573b6c2323e9210718b05351cb5
Xiaoyang Kao <kaoxiaoyang@gmail.com>
2013-03-30 12:01:55 +0800

    Integrated with serial line

--- sketch_mar24a/sketch_mar24a.ino
+++ sketch_mar24a/sketch_mar24a.ino
@@ -1,11 +1,15 @@
 float temp = 20.1;
-int8_t motor_speed[] = {200, 124, 233, 55, 23, 53};
-boolean motor_dir[]= {1, 0, 1, 1, 0, 1};
-int8_t camera_tilt[] = {125, 200};
-int8_t camera_pan[] = {30, 122};
-int8_t imu[] = {125, 125, 125}; // roll, yaw, pitch
-float depth = 2.5;
+int motor_speed[] = {200, 124, 233, 55, 23, 53};
+boolean motor_dir[]= {1, 0, 1, 1, 0, 1}; // 1 forward, 0 rev
+uint8_t camera_tilt[] = {125, 200};
+uint8_t camera_pan[] = {30, 122};
+uint8_t imu[] = {125, 125, 125}; // roll, yaw, pitch
 
+float depth = 2.5;
+float psi = 14.5;
+int8_t humidity = 50;
+float voltage = 12.53;
+float current = 10.5;
 
 void setup() {
  Serial.begin(9600); 
@@ -19,7 +23,17 @@ void loop() {
   for(int x = 0; x < 2; x++) { Serial.print(camera_tilt[x]); Serial.print(','); }
   for(int x = 0; x < 2; x++) { Serial.print(camera_pan[x]); Serial.print(','); }
   for(int x = 0; x < 3; x++) { Serial.print(imu[x]); Serial.print(','); }
-  Serial.println(depth);
+  Serial.print(depth);
+  Serial.print(',');
+  Serial.print(psi);
+  Serial.print(',');  
+  Serial.print(humidity);
+  Serial.print(',');  
+  Serial.print(voltage);
+  Serial.print(',');    
+  Serial.print(current);
+  Serial.print(',');    
+  
   delay(500);
 
   
